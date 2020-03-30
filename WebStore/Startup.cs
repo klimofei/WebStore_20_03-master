e14ccb229b480a95086cac23b51bfae480f7960f
Klim Krivosheev <klim.msu@gmail.com>
2020-03-30 12:45:59 +0300

    Создана область администратора

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -108,6 +108,11 @@ namespace WebStore
                 endpoints.MapControllerRoute(
                     name: "default", 
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
