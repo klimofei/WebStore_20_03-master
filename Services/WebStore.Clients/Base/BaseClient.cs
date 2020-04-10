054fa8beeee04454a5455cb6203951e7d8f42741
Klim Krivosheev <klim.msu@gmail.com>
2020-04-10 13:21:30 +0300

    Клиент сервиса WebAPI для сотрудников

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 namespace WebStore.Clients.Base
@@ -20,6 +22,40 @@ namespace WebStore.Clients.Base
                 BaseAddress = new Uri(Configuration["WebApiURL"])
             };
             _Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        protected T Get<T>(string url) where T : new() => GetAsync<T>(url).Result;
+
+        protected async Task<T> GetAsync<T>(string url, CancellationToken Cancel = default) where T : new()
+        {
+            var response = await _Client.GetAsync(url, Cancel);
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadAsAsync<T>(Cancel);
+
+            return new T();
+        }
+
+        protected HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item, CancellationToken Cancel = default)
+        {
+            var response = await _Client.PostAsJsonAsync(url, item, Cancel);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item, CancellationToken Cancel = default)
+        {
+            var response = await _Client.PutAsJsonAsync(url, item, Cancel);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+
+        protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken Cancel = default)
+        {
+            return await _Client.DeleteAsync(url, Cancel);
         }
 
         #region IDisposable
