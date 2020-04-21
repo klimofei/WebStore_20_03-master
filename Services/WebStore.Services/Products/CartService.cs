0d276b66b2dfc731064ba5bf67c8aa96005859cb
Klim Krivosheev <klim.msu@gmail.com>
2020-04-21 14:23:07 +0300

    Модульные тесты для основных методов сервиса корзины

--- Services/WebStore.Services/Products/CartService.cs
+++ Services/WebStore.Services/Products/CartService.cs
@@ -24,7 +24,7 @@ namespace WebStore.Services.Products
             var item = cart.Items.FirstOrDefault(i => i.ProductId == id);
 
             if (item is null)
-                cart.Items.Add(new CartItem { ProductId = id, Quantity = 1 });
+                cart.Items.Add(new CartItem {ProductId = id, Quantity = 1});
             else
                 item.Quantity++;
 
@@ -67,22 +67,23 @@ namespace WebStore.Services.Products
         {
             var cart_items = _CartStore.Cart.Items;
             var products = _ProductData
-               .GetProducts(new ProductFilter
-               {
-                   Ids = cart_items.Select(item => item.ProductId).ToList()
-               })
-               .Select(p => p.FromDTO())
-               .ToView()
-               .ToDictionary(p => p.Id);
+                .GetProducts(new ProductFilter
+                {
+                    Ids = cart_items.Select(item => item.ProductId).ToList()
+                })
+                .Select(p => p.FromDTO())
+                .ToView()
+                .ToDictionary(p => p.Id);
 
             return new CartViewModel
             {
                 Items = cart_items
-                   .Where(item => products.ContainsKey(item.ProductId))
-                   .ToDictionary(
+                    .Where(item => products.ContainsKey(item.ProductId))
+                    .ToDictionary(
                         item => products[item.ProductId],
                         item => item.Quantity
                     )
             };
         }
     }
+}
