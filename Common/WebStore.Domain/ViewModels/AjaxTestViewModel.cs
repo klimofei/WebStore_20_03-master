eb1654948b7c1d2a451de18ce0d5de731e4ba072
Klim Krivosheev <klim.msu@gmail.com>
2020-05-04 16:48:27 +0300

    Асинхронные вызовы к серверу с использованием ненавязчивого Ajax

---
+++ Common/WebStore.Domain/ViewModels/AjaxTestViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class AjaxTestViewModel
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime ServerTime { get; set; }
+    }
+}
